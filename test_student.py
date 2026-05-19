f3457dda65876398954921042677c4ea29441b5e
benchchung <bchung28@calpoly.edu>
2026-05-18 18:00:59 -0700

    created insert test

--- test_student.py
+++ test_student.py
@@ -7,6 +7,13 @@ class TestBenStudent(unittest.TestCase):
         result = heapify_up(heap, 1)
         self.assertEqual(result.data[0].freq, 3)
 
+    def test_insert(self):
+        heap = MinHeap()
+        heap = insert(heap, Node(5, "a"))
+        self.assertEqual(heap.data[0].freq, 5)
+        self.assertEqual(len(heap.data), 1)
+
+
 
 
 if __name__ == "__main__":
