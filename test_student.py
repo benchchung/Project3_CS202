7bcf9505bdb0e5730c2c1d2604a73c3848fcc6a4
benchchung <bchung28@calpoly.edu>
2026-05-18 17:56:44 -0700

    created heapify up test

--- test_student.py
+++ test_student.py
@@ -3,7 +3,10 @@ from proj3 import *
 
 class TestBenStudent(unittest.TestCase):
     def test_heapify_up(self):
-        heap = MinHeap()
+        heap = MinHeap([Node(5, "a"), Node(3, "b")])
+        result = heapify_up(heap, 1)
+        self.assertEqual(result.data[0].freq, 3)
+
 
 
 if __name__ == "__main__":
